07a52eb623167893ea11091a1f6a307787d2e7d7
Kim JaeSeok <iai6203@gmail.com>
2024-05-22 14:28:16 +0900

    🔥

---
+++ Python/problems/programmers_단어_변환/solution.py
@@ -0,0 +1,44 @@
+# 프로그래머스 - 단어 변환
+#
+# https://school.programmers.co.kr/learn/courses/30/lessons/43163
+
+
+import collections
+
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+
+    def bfs():
+        answer = 0
+        queue = collections.deque()
+        visited = [False for _ in range(len(words))]
+
+        queue.append((begin, 0))
+
+        while len(queue) > 0:
+            current_word, current_count = queue.popleft()
+
+            if current_word == target:
+                answer = current_count
+                break
+
+            for i, word in enumerate(words):
+                if visited[i] == True:
+                    continue
+
+                count = 0
+                for position in range(len(current_word)):
+                    if current_word[position] != word[position]:
+                        count += 1
+
+                    if count > 1:
+                        break
+
+                if count == 1:
+                    queue.append((word, current_count + 1))
+
+        return answer
+
+    return bfs()
