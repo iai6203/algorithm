70cc74230df90a726d21782a1117c435bb4bc013
Kim JaeSeok <iai6203@gmail.com>
2024-05-27 14:31:18 +0900

    🔥

--- Python/problems/programmers_올바른_괄호/solution.py
+++ Python/problems/programmers_올바른_괄호/solution.py
@@ -2,29 +2,15 @@
 #
 # https://school.programmers.co.kr/learn/courses/30/lessons/12909
 
-import collections
-
-
 def solution(s):
-    q = collections.deque()
+    stack = []
 
     for i in range(len(s)):
-        if s[i] == "(" or s[i] == ")":
-            q.append(s[i])
-
-    # 괄호 개수가 홀수라면 잘못된 것!
-    if len(q) % 2 == 1:
-        return False
-
-    stack = []
-    while q:
-        s1 = q.popleft()
-
-        if s1 == "(":
-            stack.append(s1)
+        if s[i] == "(":
+            stack.append(s[i])
             continue
 
-        if s1 == ")":
+        if s[i] == ")":
             if len(stack) == 0:
                 return False
 
