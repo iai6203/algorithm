d97a38b07d9d58862d14d502645883f5829cf23e
Kim JaeSeok <iai6203@gmail.com>
2024-06-09 13:49:56 +0900

    feat: 🔥

---
+++ Python/problems/programmers_점프와_순간_이동/solution.py
@@ -0,0 +1,15 @@
+# 프로그래머스 - 점프와 순간 이동
+#
+# https://school.programmers.co.kr/learn/courses/30/lessons/12980
+
+def solution(n):
+    e = 0
+
+    while n > 0:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n -= 1
+            e += 1
+
+    return e
