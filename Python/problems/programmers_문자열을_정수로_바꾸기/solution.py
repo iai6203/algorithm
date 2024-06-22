21464c1249bccedae23b16131659a734d468ac92
Kim JaeSeok <iai6203@gmail.com>
2024-06-22 17:36:20 +0900

    feat: 🔥

---
+++ Python/problems/programmers_문자열을_정수로_바꾸기/solution.py
@@ -0,0 +1,10 @@
+# 프로그래머스 - 문자열을 정수로 바꾸기
+#
+# https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+def solution(s):
+    return int(s)
+
+
+print(solution("1234"))
+print(solution("-1234"))
